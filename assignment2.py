5a1ef8efef7f5315f1a0ca174ef4e6a76b7768cd
nikki-w <114361108+nikki-w@users.noreply.github.com>
2025-02-17 16:09:00 +0000

    Added print funct for vector in spherica
    This uses the inherited class

--- assignment2.py
+++ assignment2.py
@@ -3,6 +3,9 @@
 of performing operations with vectors"""
 import assignment2_class as vect
 
+print("\b")
+print("Cartesian:")
+
 v1 = vect.Vector(2, 10, 7)
 print("First vector", v1)
 
@@ -14,3 +17,20 @@ print("Subtraction of vectors", v1 - v2)
 print("Magnitude of first vector", v1.norm())
 print("Dot product of two vectors", v1.dot(v2))
 print("Cross product of two vectors", v1.cross(v2))
+
+
+
+print("\b")
+print("Spherical Polar:")
+
+v1_sp = vect.Spherical_Polar.spherical_conv(v1)
+print("First vector", v1_sp)
+
+v2_sp = vect.Spherical_Polar.spherical_conv(v2)
+print("Second vector", v2_sp)
+
+#print("Sum of vectors:", v1_sp + v2_sp)
+#print("Subtraction of vectors", v1_sp - v2_sp)
+#print("Magnitude of first vector", v1_sp.norm())
+#print("Dot product of two vectors", v1_sp.dot(v2))
+#print("Cross product of two vectors", v1_sp.cross(v2))
