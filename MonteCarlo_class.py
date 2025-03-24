410907a50985f9ff12b9f7a7794eec6386796463
nikki-w <114361108+nikki-w@users.noreply.github.com>
2025-03-24 15:24:56 +0000

    Fixed indentation and argument issues
    Corrected indentation in the __init__ of the MC class and fixed the points function from having D, N, D_low and D_high to be prefixed with 'self.' to be in keeping with class system

--- MonteCarlo_class.py
+++ MonteCarlo_class.py
@@ -5,18 +5,22 @@ import numpy as np
 from mpi4py import MPI
 
 class MC:
-    def __init__(self, N, D, D_low, D_high):
-    """
-    Initialsie Monte Carlo (MC)
+    def __init__(self, N, D, D_low=-1.0, D_high=1.0):
+        """
+        Initialsie Monte Carlo (MC)
         
-    N - Number of samples
-    D - Number of dimensions for each point
-    D_low - Lower limit for each dimension
-    D_high - Upper limit for each dimension
-    """
-    self.N = N
-    self.D = D
-    self.D_low = D_low
-    self.D_high = D_high
+        N - Number of samples
+        D - Number of dimensions for each point
+        D_low - Lower limit for each dimension
+        D_high - Upper limit for each dimension
+        """
+        self.N = N
+        self.D = D
+        self.D_low = D_low
+        self.D_high = D_high
+
+    def points(self):
+        """Generates random points with D being the number of dimensions"""
+        return np.random.uniform(self.D_low, self.D_high, size=(self.N, self.D))
 
 
