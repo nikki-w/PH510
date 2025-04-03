92b0123cea2e1b9071f7e661687d335c212d1877
nikki-w <114361108+nikki-w@users.noreply.github.com>
2025-04-03 13:43:40 +0100

    Most recent version to help with mpirun issues
    Upload of recent version to help debug mpirun issues im having

--- assignment3.py
+++ assignment3.py
@@ -8,12 +8,14 @@ import montecarlo_class as MC
 # Initialise MC in 2D
 MC_rand = MC.MC(d=2, n=1000000000, seed=134)
 
-# Calculate exact volume for D case 
+# Calculate exact volume for D case
 volume_2D_exact = MC_rand.volume_exact()
 
 # Calculate estimated volume using MC
 volume_2D_MC = MC_rand.volume(r=1)
+volume_2D_MC_mean, volume_2D_MC_err = MC_rand.statistics(volume_2D_MC)
 
 if MC_rand.rank == 0:
     print(f"Exact volume: {volume_2D_exact:.6f}")
-    print(f"MC estimate: {volume_2D_MC:.6f}") 
+    print(f"MC estimate: {volume_2D_MC:.6f} +/- {volume_2D_MC_err:.6f}")
+    #print(f"Relative error: {abs(volume_2D_MC - volume_2D_exact)/volume_2D_exact*100:.2f}%")
