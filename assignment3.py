619193f8a9c751db7bfe3ef2214010d42a381977
nikki-w <114361108+nikki-w@users.noreply.github.com>
2025-04-05 14:24:46 +0100

    Reduced number of samples
    By reducing number of samples, I fixed the error I was getting when submitting my .sh file (turns out it was due to a memory issue), so now i'm getting the right output when running my .sh file

--- assignment3.py
+++ assignment3.py
@@ -6,7 +6,7 @@ File that uses MC class"""
 import montecarlo_class as MC
 
 # Initialise MC in 2D
-MC_rand = MC.MC(d=2, n=1000000000, seed=134)
+MC_rand = MC.MC(d=2, n=100000000, seed=134)
 
 # Calculate exact volume for D case
 volume_2D_exact = MC_rand.volume_exact()
@@ -17,5 +17,5 @@ volume_2D_MC_mean, volume_2D_MC_err = MC_rand.statistics(volume_2D_MC)
 
 if MC_rand.rank == 0:
     print(f"Exact volume: {volume_2D_exact:.6f}")
-    print(f"MC estimate: {volume_2D_MC:.6f} +/- {volume_2D_MC_err:.6f}")
+    print(f"MC estimate: {volume_2D_MC:.6f} +/- {volume_2D_MC_err:.10f}")
     #print(f"Relative error: {abs(volume_2D_MC - volume_2D_exact)/volume_2D_exact*100:.2f}%")
